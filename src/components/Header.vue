06fc44043fb9a32cab3daebeed108946ef2782ad
LJW <dementor0711@gmail.com>
2024-08-06 16:38:36 +0900

    05. Header - Logo 및 Google Fonts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <header>
+    <Logo></Logo>
     <div class="nav nav-pills">
       <div v-for="nav in navigations"
            :key="nav.name"
@@ -17,7 +18,10 @@
 
 
 <script>
+import Logo from "./Logo.vue";
+
 export default {
+  components: {Logo},
   data() {
     return {
       navigations: [
@@ -38,3 +42,14 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+header{
+  height: 70px;
+  padding: 0 40px;
+  display: flex;
+  align-items: center;
+  .logo{
+    margin-right : 40px;
+  }
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <RouterLink to="/" class="logo">
+    <span>OMDbApi</span>.COM
+  </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+
+.logo {
+  font-family: "East Sea Dokdo", sans-serif;
+  font-size: 20px;
+  color: $black;
+  &:hover{
+    color:$black;
+  }
+  span{
+    color:$primary;
+  }
+}
+</style>
